cbaeea5ee1e6b9c1a2167796b71d01f651ad598a
Revathyviswa <hemarev05@gmail.com>
2016-07-16 17:19:25 +0530

    Add files via upload

---
+++ Rev.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rev;
+
+/**
+ *
+ * @author Student
+ */
+public class Rev {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for(int i=10;i<=20;i++)
+       {
+           if(i%2==1)
+           {
+               System.out.println(i);
+           }
+               }
+    }
+}
